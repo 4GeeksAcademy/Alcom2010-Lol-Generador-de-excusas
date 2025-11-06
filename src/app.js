11ecdcd5eaf67b50932bc594ca4c399f39ef14c4
Jose Soyano <joselsoyano@gmail.com>
2025-11-06 21:45:32 +0000

    Generador de excusas Alfa

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+// Arrays de palabras
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+// Función para elegir elemento aleatorio
+function randomFrom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+  const excuse = `${randomFrom(who)} ${randomFrom(action)} ${randomFrom(what)} ${randomFrom(when)}.`;
+  document.getElementById('excuse').textContent = excuse;
+});
