1e9b735018ecf22a28b8b4de84388c9d0c0eabe9
Jose Soyano <joselsoyano@gmail.com>
2025-11-09 00:38:55 +0000

    Generador 2.0

--- src/app.js
+++ src/app.js
@@ -12,10 +12,11 @@ window.onload = function() {
 
 
 // Arrays de palabras
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let who = ['Mi gato', 'Mi perro', 'Mi abuela', 'El cartero', 'Mi profesor', 'Mi vecino', 'Un extraterrestre', 'El fantasma del baño', 'Mi hermano pequeño', 'Un payaso en bicicleta', 'Mi jefe', 'El repartidor de pizza', 'Una paloma gigante', 'Mi pez dorado', 'Mi mejor amigo'];
+let action = ['se comió', 'rompió', 'escondió', 'robó', 'quemó', 'tiró por la ventana', 'mojó', 'bailó sobre', 'pintó', 'se durmió encima de', 'gritó a', 'pegó con cinta adhesiva', 'confundió con comida', 'olvidó', 'transformó en confeti'];
+let what = ['mi tarea', 'mi teléfono', 'mi ordenador', 'mi coche', 'mi almuerzo', 'mi cuaderno', 'mi mochila', 'mi cama', 'mi libro favorito', 'mi proyecto final', 'mi guitarra', 'mi zapato', 'mi diploma', 'mi control remoto', 'mi uniforme'];
+let when = ['antes de venir a clase', 'cuando me estaba duchando', 'mientras dormía', 'cuando estaba cocinando', 'durante la tormenta', 'cuando iba de camino al trabajo', 'mientras veía la tele', 'cuando estaba rezando', 'justo antes de salir de casa', 'cuando estaba haciendo ejercicio', 'durante la siesta', 'mientras jugaba con mi perro', 'cuando se fue la luz', 'cuando estaba estudiando', 'mientras lavaba los platos'];
+
 
 // Función para elegir elemento aleatorio
 function randomFrom(arr) {
